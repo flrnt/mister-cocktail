0d6ec8856f59dfbdc2461706f55c67d0c397475c
Florent Allain <allain.florent@gmail.com>
2015-08-20 15:15:27 +0200

    added ui components to embed pages

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,27 @@
 class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [:show, :create]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+  def show
+
+  end
+  def new
+
+  end
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+
+Cocktail.create(name: "Old Fashioned")
+Cocktail.create(name: "Daiquiri")
+Cocktail.create(name: "Margarita")
+Cocktail.create(name: "Sidecar")
+Cocktail.create(name: "French 75")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Irish Coffee")
+Cocktail.create(name: "Jack Rose")
+Cocktail.create(name: "Negroni")
+Cocktail.create(name: "Boulevardier")
+Cocktail.create(name: "Sazerac")
+Cocktail.create(name: "Vieux Carré")
+Cocktail.create(name: "Ramos Gin Fizz")
+Cocktail.create(name: "Mint Julep")
+Cocktail.create(name: "Whiskey Sour")
+Cocktail.create(name: "Mai Tai")
+Cocktail.create(name: "Planter's Punch")
+Cocktail.create(name: "Pisco Sour")
+Cocktail.create(name: "Cosmopolitan")
+Cocktail.create(name: "Tom Collins")
+Cocktail.create(name: "Last Word")
+
+
+
+
+
+
+
