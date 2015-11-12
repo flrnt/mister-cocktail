465afd6a7f28cc8de7157d2d7bdfc10ea12fa7cc
Florent Allain <allain.florent@gmail.com>
2015-11-12 10:16:20 +0100

    add debug gems and nested routes

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ gem 'simple_form'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'binding_of_caller'
+  gem 'better_errors'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,11 +1,26 @@
 class DosesController < ApplicationController
-  def new
 
+  before_action :find_cocktail
+
+  def new
+    @dose = Dose.new
   end
+
   def create
 
   end
+  
   def destroy
+
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
 
   end
 end
